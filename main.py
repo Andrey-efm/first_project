bb5314c7c1e189c4ff02fbe64b19e00530e70750
Andrey Efimkin <andryuhaefimkin@yandex.ru>
2026-05-01 16:14:12 +0300

    Добавить print_author() с чтением из .env

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+def print_author():
+    # Загружаем переменные из файла .env
+    load_dotenv()
+    
+    # Читаем значение переменной AUTHOR
+    author = os.getenv("AUTHOR")
+    
+    # Выводим имя автора
+    print(f"Автор проекта: {author}")
+
+# Вызов функции для проверки
+if __name__ == "__main__":
+    print_author()
+
